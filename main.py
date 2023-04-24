770f44a7c595bd63a66b71ea8eaab0a0585a4de5
Isaac Robert <owoyemiisaac33@gmail.com>
2023-04-24 15:27:14 +0100

    ...

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
+import os, sys
 import speech_recognition as sr
 from dotenv import load_dotenv
 from utils import fetch_response, listen_to_speech, recognize_speech, speak_text
 
 load_dotenv()
+args = sys.argv[1:]
 
 API_KEY = os.environ["OPENAI_API_KEY"]
 model = "text-davinci-002"
@@ -16,14 +17,19 @@ def initialize_sr():
     mic = sr.Microphone()
 
 
-initialize_sr()
-print("[*] Listening to speech...")
-audio = listen_to_speech(mic=mic, r=r)
-print("[*] Running speech recognition on speech...")
-try:
-    prompt = recognize_speech(r, audio=audio)
-except:
-    print("Error recognizing speech")
+if "-t" in args:
+    prompt = input("Enter a prompt: ")
+else:
+    initialize_sr()
+    print("[*] Listening to speech...")
+    audio = listen_to_speech(mic=mic, r=r)
+
+    print("[*] Running speech recognition on speech...")
+    try:
+        prompt = recognize_speech(r, audio=audio)
+    except:
+        print("Error recognizing speech")
+
 print(f"[*] Fetching response to prompt ({prompt})...")
 response = fetch_response(API_KEY, model, prompt=prompt)
 print("[*] Running speech syntheis for response")
